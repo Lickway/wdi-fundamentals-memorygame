ed56df9f51c618b2fd361bbb3a8b74a31419cd41
unknown <liekwegmedia@gmail.com>
2018-02-08 18:01:30 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-if (cardsInPlay[0] === cardsInPlay[1]) { 
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');
-}
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+flipCard(0);
+flipCard(2);
